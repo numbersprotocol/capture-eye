5ac244aa0b6936cf1ff03c699844d34fd7daf0de
shc261392 <james@numbersprotocol.io>
2024-09-09 16:14:18 +0800

    fix: fix image loading issue caused by set imageLoaded to false in resetModalProps (#25)
    * fix: fix image loading issue caused by set imageLoaded to false in resetModalProps
    
    Signed-off-by: James Chien <james@numbersprotocol.io>
    
    * fix(src/modal/modal.ts): remove unneeded code to reset eng img/link and action text/link
    
    Signed-off-by: James Chien <james@numbersprotocol.io>
    
    ---------
    
    Signed-off-by: James Chien <james@numbersprotocol.io>

--- src/modal/modal.ts
+++ src/modal/modal.ts
@@ -46,15 +46,26 @@ export class CaptureEyeModal extends LitElement {
     if (setAsLoaded) this._assetLoaded = true;
   }
 
+  updateEngagementZone(
+    engagementImage: string,
+    engagementLink: string,
+    actionButtonText: string,
+    actionButtonLink: string
+  ) {
+    if (this.engagementImage !== engagementImage) {
+      this._imageLoaded = false;
+      this.engagementImage = engagementImage;
+      this.requestUpdate();
+    }
+    this.engagementLink = engagementLink;
+    this.actionButtonText = actionButtonText;
+    this.actionButtonLink = actionButtonLink;
+  }
+
   resetModalProps() {
-    this.engagementImage = Constant.url.defaultEngagementImage;
-    this.engagementLink = Constant.url.defaultEngagementLink;
-    this.actionButtonText = '';
-    this.actionButtonLink = '';
     this._blockchain = Constant.text.numbersMainnet;
     this._asset = undefined;
     this._assetLoaded = false;
-    this._imageLoaded = false;
   }
 
   override firstUpdated() {
@@ -270,6 +281,7 @@ export class CaptureEyeModal extends LitElement {
 
   private handleImageLoad() {
     this._imageLoaded = true;
+    console.log('imageLoaded', this._imageLoaded);
   }
 
   override render() {
