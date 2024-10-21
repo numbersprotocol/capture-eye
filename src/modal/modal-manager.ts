0626fb0656bb2cf8d9aba52e36e837b5162d87a7
shc261392 <james@numbersprotocol.io>
2024-10-21 15:17:41 +0800

    feat: refactoring and add copyrightZoneTitle attribute for customization (#35)
    * feat: refactoring and add copyrightZoneTitle attribute for customization
    
    Signed-off-by: James Chien <james@numbersprotocol.io>
    
    * test(src/test/modal_test.ts): add more tests
    
    Signed-off-by: James Chien <james@numbersprotocol.io>
    
    * docs(README.md): add description for cz-title
    
    Signed-off-by: James Chien <james@numbersprotocol.io>
    
    * fix(src/{capture-eye,modal/modal}.ts): fix engagement link default value and tracking
    
    Signed-off-by: James Chien <james@numbersprotocol.io>
    
    * fix(src/modal/modal.ts): reset imageLoaded state when eng zone image changed
    
    Signed-off-by: James Chien <james@numbersprotocol.io>
    
    ---------
    
    Signed-off-by: James Chien <james@numbersprotocol.io>

--- src/modal/modal-manager.ts
+++ src/modal/modal-manager.ts
@@ -1,4 +1,4 @@
-import { CaptureEyeModal } from './modal.js';
+import { CaptureEyeModal, ModalOptions } from './modal.js';
 import { AssetModel } from '../asset/asset-model.js';
 import { fetchAsset, hasNftProduct } from '../asset/asset-service.js';
 import interactionTracker, { TrackerEvent } from './interaction-tracker.js';
@@ -19,98 +19,43 @@ export class ModalManager {
     return this?.modalElement?.modalHidden ?? true;
   }
 
-  public static getInstance(): ModalManager {
+  static getInstance(): ModalManager {
     if (!ModalManager.instance) {
       ModalManager.instance = new ModalManager();
     }
     return ModalManager.instance;
   }
 
-  public initializeModal(): void {
+  initializeModal(): void {
     if (!this.modalElement) {
       this.modalElement = document.createElement('capture-eye-modal');
       document.body.appendChild(this.modalElement);
     }
   }
 
-  public updateModal(
-    nid: string,
-    layout: string,
-    engagementImage: string,
-    engagementLink: string,
-    actionButtonText: string,
-    actionButtonLink: string,
-    position: { top: number; left: number }
-  ): void {
-    if (this.modalElement) {
-      this.positionModal(position);
-      this.modalElement.modalHidden = false;
-      this.registerRootClickListener();
-
-      if (this.modalElement.nid !== nid) {
-        this.modalElement.resetModalProps();
-        this.modalElement.nid = nid;
-        this.modalElement.layout = layout;
-        this.modalElement.updateEngagementZone(
-          engagementImage,
-          engagementLink,
-          actionButtonText,
-          actionButtonLink
-        );
-        fetchAsset(nid).then((assetData) =>
-          this.updateModalAsset(assetData, true)
-        );
-        hasNftProduct(nid).then((hasNftProduct) =>
-          this.updateModalAsset({ hasNftProduct }, false)
-        );
-      }
-    }
-  }
-
-  public updateModalWithDelay(
-    nid: string,
-    layout: string,
-    engagementImage: string,
-    engagementLink: string,
-    actionButtonText: string,
-    actionButtonLink: string,
-    position: { top: number; left: number }
-  ): void {
+  async updateModal(options: ModalOptions, delay = 150): Promise<void> {
     if (!this.modalElement) return;
     this.modalElement.modalHidden = true;
-    setTimeout(
-      () =>
-        this.updateModal(
-          nid,
-          layout,
-          engagementImage,
-          engagementLink,
-          actionButtonText,
-          actionButtonLink,
-          position
-        ),
-      150
-    );
-  }
+    await new Promise((resolve) => setTimeout(resolve, delay));
+    const nidChanged = this.modalElement.nid !== options.nid;
+    if (nidChanged) {
+      this.modalElement.clearModalOptions();
+    }
+    this.modalElement.modalHidden = false;
+    this.registerRootClickListener();
+    this.modalElement.updateModalOptions(options);
 
-  private remToPixels(rem: number): number {
-    return (
-      rem * parseFloat(getComputedStyle(document.documentElement).fontSize)
-    );
-  }
-
-  private positionModal(position: { top: number; left: number }): void {
-    if (this.modalElement) {
-      const remOffset = this.remToPixels(1); // Convert 1rem to pixels
-
-      // Update modal position styles with 1rem offset
-      this.modalElement.style.position = 'absolute';
-      this.modalElement.style.top = `${position.top + remOffset}px`;
-      this.modalElement.style.left = `${position.left + remOffset}px`;
+    if (nidChanged) {
+      fetchAsset(options.nid).then((assetData) =>
+        this.updateModalAsset(assetData, true)
+      );
+      hasNftProduct(options.nid).then((hasNftProduct) =>
+        this.updateModalAsset({ hasNftProduct }, false)
+      );
     }
   }
 
-  public hideModal(): void {
+  hideModal(): void {
     if (this.modalElement) {
       this.modalElement.modalHidden = true;
       this.unregisterRootClickListener();
