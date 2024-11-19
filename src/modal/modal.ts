e504623d1f708ee425f2b3a41ac44088e7cfec0f
James Chien <james@numbersprotocol.io>
2024-11-19 12:53:42 +0800

    fix(src/modal/modal.ts): change audio icon
    Signed-off-by: James Chien <james@numbersprotocol.io>

--- src/modal/modal.ts
+++ src/modal/modal.ts
@@ -268,15 +268,29 @@ export class CaptureEyeModal extends LitElement {
       : typeof this._asset?.encodingFormat === 'string' &&
         this._asset.encodingFormat.includes('audio/')
       ? html`<svg
-          class="profile-img"
           xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 384 512"
+          version="1.1"
+          xmlns:xlink="http://www.w3.org/1999/xlink"
+          xmlns:svgjs="http://svgjs.com/svgjs"
+          width="100"
+          height="100"
+          x="0"
+          y="0"
+          viewBox="0 0 512 512"
+          style="enable-background:new 0 0 100 100"
+          xml:space="preserve"
+          class="profile-img"
         >
-          <!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
-          <path
-            fill="#027FE5"
-            d="M64 464l256 0c8.8 0 16-7.2 16-16l0-288-80 0c-17.7 0-32-14.3-32-32l0-80L64 48c-8.8 0-16 7.2-16 16l0 384c0 8.8 7.2 16 16 16zM0 64C0 28.7 28.7 0 64 0L229.5 0c17 0 33.3 6.7 45.3 18.7l90.5 90.5c12 12 18.7 28.3 18.7 45.3L384 448c0 35.3-28.7 64-64 64L64 512c-35.3 0-64-28.7-64-64L0 64zM192 272l0 128c0 6.5-3.9 12.3-9.9 14.8s-12.9 1.1-17.4-3.5L129.4 376 112 376c-8.8 0-16-7.2-16-16l0-48c0-8.8 7.2-16 16-16l17.4 0 35.3-35.3c4.6-4.6 11.5-5.9 17.4-3.5s9.9 8.3 9.9 14.8zm85.8-4c11.6 20 18.2 43.3 18.2 68s-6.6 48-18.2 68c-6.6 11.5-21.3 15.4-32.8 8.8s-15.4-21.3-8.8-32.8c7.5-12.9 11.8-27.9 11.8-44s-4.3-31.1-11.8-44c-6.6-11.5-2.7-26.2 8.8-32.8s26.2-2.7 32.8 8.8z"
-          />
+          <g>
+            <path
+              d="M501.969 5.638A23.063 23.063 0 0 0 483.603.231L160.83 46.341c-11.359 1.623-19.795 11.351-19.795 22.825V339.475c-13.656-7.956-29.509-12.537-46.421-12.537-51.021 0-92.531 41.51-92.531 92.531S43.592 512 94.613 512s92.531-41.51 92.531-92.531V181.383l276.663-39.523v151.504c-13.656-7.956-29.509-12.537-46.421-12.537-51.021 0-92.531 41.51-92.531 92.531 0 51.021 41.51 92.531 92.531 92.531s92.531-41.51 92.531-92.531V23.055a23.05 23.05 0 0 0-7.948-17.417z"
+              fill="#808080"
+              data-original="#000000"
+              data-darkreader-inline-fill=""
+              style="--darkreader-inline-fill: #292a2b;"
+              class=""
+            ></path>
+          </g>
         </svg>`
       : html`<img
           src=${Constant.url.blankThumbnail}
