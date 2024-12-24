958f3e84700f666c0af25dd1a71cc419bd14ed8c
James Chien <james@numbersprotocol.io>
2024-12-24 14:10:12 +0800

    fix(scripts/deploy-release.sh): avoid bumping version in deploy script and reuse keys
    Signed-off-by: James Chien <james@numbersprotocol.io>

--- scripts/deploy-release.sh
+++ scripts/deploy-release.sh
@@ -7,13 +7,12 @@ S3_BUCKET="numbers-static"
 
 # Define S3 keys for paths
 LATEST_KEY="capture-eye/release/latest/capture-eye.bundled.js"
-VERSION_KEY="capture-eye/release/$(npm version --no-git-tag-version patch)/capture-eye.bundled.js"
+VERSION_KEY="capture-eye/release/$(npm pkg get version | tr -d \")/capture-eye.bundled.js"
 
 # CloudFront invalidation paths
-INVALIDATION_PATHS=(
-  "/capture-eye/release/latest/capture-eye.bundled.js"
-  "/capture-eye/release/$(npm version --no-git-tag-version patch)/capture-eye.bundled.js"
-)
+INVALIDATION_PATHS="/$LATEST_KEY /$VERSION_KEY"
+
+echo "Invalidation paths: $INVALIDATION_PATHS"
 
 # Upload to S3 (latest version)
 aws s3 cp $FILE_PATH s3://$S3_BUCKET/$LATEST_KEY
@@ -31,11 +30,8 @@ if [ $? -ne 0 ]; then
 fi
 echo "File uploaded to s3://$S3_BUCKET/$VERSION_KEY"
 
-# Create invalidation paths string
-INVALIDATION_PATHS_STRING=$(printf ' "%s"' "${INVALIDATION_PATHS[@]}")
-
 # Create the invalidation
-INVALIDATION_ID=$(aws cloudfront create-invalidation --distribution-id $DISTRIBUTION_ID --paths $INVALIDATION_PATHS_STRING --query 'Invalidation.Id' --output text)
+INVALIDATION_ID=$(aws cloudfront create-invalidation --distribution-id $DISTRIBUTION_ID --paths $INVALIDATION_PATHS --query 'Invalidation.Id' --output text)
 if [ $? -ne 0 ]; then
   echo "Failed to create invalidation"
   exit 1
